ecb504604e076e3eb9d0879ef81756d7681a96fc
cultcargo <ivan.seleznev@dremio.com>
2025-02-07 01:54:39 +0000

    use deque push at least every 10 seconds do not wait for pop before supplier()
    Change-Id: Ic7869df33d4027e27cedc74f37ef49e6f12c28bb

--- odbcabstraction/include/odbcabstraction/blocking_queue.h
+++ odbcabstraction/include/odbcabstraction/blocking_queue.h
@@ -10,8 +10,12 @@
 #include <mutex>
 #include <condition_variable>
 #include <thread>
-#include <vector>
+#include <deque>
+#include <chrono>
 #include <boost/optional.hpp>
+
+
+#include <os/log.h>
 
 namespace driver {
 namespace odbcabstraction {
@@ -21,40 +25,51 @@ template<typename T>
 class BlockingQueue {
 
   size_t capacity_;
-  std::vector<T> buffer_;
-  size_t buffer_size_{0};
-  size_t left_{0}; // index where variables are put inside of buffer (produced)
-  size_t right_{0}; // index where variables are removed from buffer (consumed)
+  std::deque<T> buffer_;
+  std::chrono::seconds timeout_ = std::chrono::seconds(10);
+  std::chrono::time_point<std::chrono::system_clock> last_push_time_;
 
   std::mutex mtx_;
   std::condition_variable not_empty_;
   std::condition_variable not_full_;
+  std::condition_variable not_closed_;
 
-  std::vector<std::thread> threads_;
-  std::atomic<size_t> active_threads_{0};
   std::atomic<bool> closed_{false};
+  bool long_time_no_pushes_{false};
+  std::vector<std::thread> threads_;
+  std::thread notifier_thread_;
+  std::atomic<size_t> active_threads_{0};
 
 public:
   typedef std::function<boost::optional<T>(void)> Supplier;
 
-  BlockingQueue(size_t capacity): capacity_(capacity), buffer_(capacity) {}
+  BlockingQueue(size_t capacity): capacity_(capacity), last_push_time_(std::chrono::system_clock::now()) {
+      notifier_thread_ = std::thread([this]() {
+        while (!closed_) {
+          std::unique_lock<std::mutex> unique_lock(mtx_);
+          not_closed_.wait_until(unique_lock, last_push_time_ + timeout_, [this](){ return closed_.load(); });
+          os_log(OS_LOG_DEFAULT, "flightsql: BQ::notifier_thread_waked");
+          if (last_push_time_ + timeout_ < std::chrono::system_clock::now()) {
+            long_time_no_pushes_ = true;
+            not_full_.notify_one();
+            last_push_time_ = std::chrono::system_clock::now();
+            os_log(OS_LOG_DEFAULT, "flightsql: BQ::notifier_thread_notified");
+          }
+        }
+      });
+    }
 
   void AddProducer(Supplier supplier) {
+    os_log(OS_LOG_DEFAULT, "flightsql: BQ::AddProducer");
     active_threads_++;
     threads_.emplace_back([=] {
       while (!closed_) {
-        // Block while queue is full
-        std::unique_lock<std::mutex> unique_lock(mtx_);
-        if (!WaitUntilCanPushOrClosed(unique_lock)) break;
-        unique_lock.unlock();
-
-        // Only one thread at a time be notified and call supplier
         auto item = supplier();
         if (!item) break;
 
         Push(*item);
       }
-
+      os_log(OS_LOG_DEFAULT, "flightsql: BQ::AddProducer exited while");
       std::unique_lock<std::mutex> unique_lock(mtx_);
       active_threads_--;
       not_empty_.notify_all();
@@ -62,28 +77,33 @@ public:
   }
 
   void Push(T item) {
+    os_log(OS_LOG_DEFAULT, "flightsql: BQ::Push");
     std::unique_lock<std::mutex> unique_lock(mtx_);
-    if (!WaitUntilCanPushOrClosed(unique_lock)) return;
+    if (!WaitUntilCanPushOrClosed(unique_lock)) {
+      os_log(OS_LOG_DEFAULT, "flightsql: BQ::Push early exit");
+      return;
+    }
 
-    buffer_[right_] = std::move(item);
-
-    right_ = (right_ + 1) % capacity_;
-    buffer_size_++;
-
+    buffer_.push_back(std::move(item));
+    last_push_time_ = std::chrono::system_clock::now();
+    long_time_no_pushes_ = false;
     not_empty_.notify_one();
+    os_log(OS_LOG_DEFAULT, "flightsql: BQ::Push exit");
   }
 
   bool Pop(T *result) {
+    os_log(OS_LOG_DEFAULT, "flightsql: BQ::Pop");
     std::unique_lock<std::mutex> unique_lock(mtx_);
-    if (!WaitUntilCanPopOrClosed(unique_lock)) return false;
+    if (!WaitUntilCanPopOrClosed(unique_lock)) {
+      os_log(OS_LOG_DEFAULT, "flightsql: BQ::Pop early exit false");
+      return false;
+    }
 
-    *result = std::move(buffer_[left_]);
-
-    left_ = (left_ + 1) % capacity_;
-    buffer_size_--;
-
+    *result = std::move(buffer_.front());
+    buffer_.pop_front();
     not_full_.notify_one();
 
+    os_log(OS_LOG_DEFAULT, "flightsql: BQ::Pop exit true");
     return true;
   }
 
@@ -94,28 +114,30 @@ public:
     closed_ = true;
     not_empty_.notify_all();
     not_full_.notify_all();
+    not_closed_.notify_all();
 
     unique_lock.unlock();
 
     for (auto &item: threads_) {
       item.join();
     }
+    notifier_thread_.join();
   }
 
 private:
   bool WaitUntilCanPushOrClosed(std::unique_lock<std::mutex> &unique_lock) {
     not_full_.wait(unique_lock, [this]() {
-      return closed_ || buffer_size_ != capacity_;
+      return closed_ || buffer_.size() < capacity_ || long_time_no_pushes_;
     });
     return !closed_;
   }
 
   bool WaitUntilCanPopOrClosed(std::unique_lock<std::mutex> &unique_lock) {
     not_empty_.wait(unique_lock, [this]() {
-      return closed_ || buffer_size_ != 0 || active_threads_ == 0;
+      return closed_ || buffer_.size() != 0 || active_threads_ == 0;
     });
 
-    return !closed_ && buffer_size_ > 0;
+    return !closed_ && buffer_.size() != 0;
   }
 };
 
